065a6d5545c1d8746e5101f2ef65b0109452578c
Seirra <sophietheopossum@yandex.ru>
2024-10-08 13:39:07 +0100

    fixes to make output python compatible

--- model_data.py
+++ model_data.py
@@ -8,11 +8,15 @@ models = []
 providers = []
 
 for _, row in df.iterrows():
+    if row['top_provider_max_completion_tokens'] > 0:
+        max_completion_tokens = row['top_provider_max_completion_tokens']
+    else:
+        max_completion_tokens = 0
     #store model
     models.append([
         row['id'],    row['name'],    row['created'],    row['description'],    row['context_length'],    row['per_request_limits'],    row['architecture.modality'],
         row['architecture.tokenizer'],    row['architecture.instruct_type'],    row['pricing.prompt'],    row['pricing.completion'],    row['pricing.image'],
-        row['pricing.request'],    row['top_provider.context_length'],    row['top_provider.max_completion_tokens'],
+        row['pricing.request'],    row['top_provider.context_length'],    max_completion_tokens,
         row['top_provider.is_moderated'],    row['preferred_provider'],    row['model_type'],
     ])
 
@@ -25,7 +29,7 @@ def list_all():
     """
     print("\nmodels:\n")
     for model in models:
-        print(f'"{model[0]}", "{model[1]}", {model[2]}, "{model[3]}", {model[4]}, {model[5]}, "{model[6]}", "{model[7]}", "{model[8]}", {model[9]}, {model[10]}, {model[11]}, {model[12]}, {model[13]}, {model[14]}, {model[15]}, "{model[16]}", "{model[17]}", [],')
+        print(f'"{model[0]}", "{model[1]}", {model[2]}, "{model[3]}", {model[4]}, "", "{model[6]}", "{model[7]}", "{model[8]}", {model[9]}, {model[10]}, {model[11]}, {model[12]}, {model[13]}, {model[14]}, {model[15]}, "{model[16]}", "{model[17]}", [],')
 
 
 
